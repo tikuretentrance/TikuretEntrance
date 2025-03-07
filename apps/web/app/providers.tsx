6f3d4823140cd63169f9816dcdee943e1fd55220
Dagwal <dagimwalelegne@gmail.com>
2025-03-07 22:56:07 +0300

    feat: disable development mode warnings

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -5,7 +5,17 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} afterSignOutUrl={'/'}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} afterSignOutUrl={'/'}
+      appearance={{
+        // elements: {
+        //   footer: "none"
+        // },
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+
+    >
       <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         {children}
       </ThemeProvider>
