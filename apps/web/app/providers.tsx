22580f72b7f12223b56e84370d878c2ba3b1d240
Dagwal <dagimwalelegne@gmail.com>
2025-03-08 11:42:54 +0300

    feat: add @clerk/themes dependency and update provider configuration

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
-
+// import { dark } from "@clerk/themes"
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} afterSignOutUrl={'/'}
@@ -13,8 +13,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
         layout: {
           unsafe_disableDevelopmentModeWarnings: true,
         },
+        // baseTheme: dark,
       }}
-      // signUpUrl="/sign-in"
+    // signUpUrl="/sign-in"
 
     >
       <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
